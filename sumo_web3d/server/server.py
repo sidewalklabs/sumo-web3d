87e9f271ccd8ff850a82756e2f33067de4b911ad
eugenevinitsky <vinitsky.eugene@gmail.com>
2018-09-03 15:58:11 -0700

    enabled sumo web3d to run in normal mode unless we give it a port to look at

--- sumo_web3d/server/server.py
+++ sumo_web3d/server/server.py
@@ -35,7 +35,7 @@ parser.add_argument(
     '--gui', action='store_true', default=False,
     help='Run sumo-gui rather than sumo. This is useful for debugging.')
 parser.add_argument(
-    '--sumo-port', dest='sumo_port', default=8813)
+    '--sumo-port', dest='sumo_port')
 
 # Base directory for sumo_web3d
 DIR = os.path.join(os.path.dirname(__file__), '..')
@@ -333,9 +333,16 @@ async def websocket_simulation_control(sumo_start_fn, task, websocket, path):
 
 
 # TraCI business logic
-def start_sumo_executable(gui, sumo_port, sumocfg_file):
-    traci.init(port=int(sumo_port), label="")
-    traci.setOrder(1)
+def start_sumo_executable(gui, sumo_args, sumocfg_file):
+    if sumo_args.sumo_port:
+        traci.init(port=int(sumo_args.sumo_port), label="")
+        traci.setOrder(1)
+    else:
+        sumoBinary = sumolib.checkBinary('sumo' if not gui else 'sumo-gui')
+        additional_args = shlex.split(sumo_args.sumo_args) if sumo_args.sumo_args else []
+        args = [sumoBinary, '-c', sumocfg_file] + additional_args
+        print('Executing %s' % ' '.join(args))
+        traci.start(args)
     traci.simulation.subscribe()
 
     # Subscribe to all traffic lights. This set of IDs should never change.
@@ -540,7 +547,7 @@ def setup_http_server(task, scenario_file, scenarios):
 def main(args):
     global current_scenario, scenarios, SCENARIOS_PATH
     task = None
-    sumo_start_fn = functools.partial(start_sumo_executable, args.gui, args.sumo_port)
+    sumo_start_fn = functools.partial(start_sumo_executable, args.gui, args)
 
     if args.configuration_file:
         # Replace the built-in scenarios with a single, user-specified one.
